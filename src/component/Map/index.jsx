6ba6c081bdad0367810ff80665414918bfacac38
CaterJo <kin7729@naver.com>
2020-01-03 21:27:14 +0900

    Byeonggyu/ Editting naver map api"

--- src/component/Map/index.jsx
+++ src/component/Map/index.jsx
@@ -33,14 +33,15 @@ class Map extends Component {
     const { ncpClientId, getDataAsync, _lat, _lng, zoomLevel } = this.props
 
     this.loadScriptPromise(ncpClientId).then((naver) => {
-      let zoomRange = [2, 4, 6]
+      let zoomRange = [6, 8, 10] 
       let maxZoom = zoomRange[zoomRange.length - 1]
       let minZoom = zoomRange[0]
 
-      // 전국 : 2, 시군구 :4  읍면동 : 7
+      // 전국 : 2, 시군구 :4  읍면동 : 6
       // naver.maps. PointBounds 경계 생성.
       // 인터렉션 옵션.
       const mapOptions = {
+        useStyleMap: true,
         logoControl: false,
         mapDataControl: false,
         scaleControl: false,
